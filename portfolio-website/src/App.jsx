84f333a7508eff28c7471ea921f87798bcf1af88
Zion Koundou <kodesone13@gmail.com>
2026-05-28 16:43:58 -0700

    themes

--- portfolio-website/src/App.jsx
+++ portfolio-website/src/App.jsx
@@ -1,9 +1,6 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import Navbar from './components/Navbar.jsx'
-import './App.css'
+
 
 function App() {
 
