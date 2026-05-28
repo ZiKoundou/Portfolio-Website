64f1b3911fe8d5f3394aa7e228b7309c6eced79b
Zion Koundou <kodesone13@gmail.com>
2026-05-28 12:54:34 -0700

    Initial vite built react app

---
+++ portfolio-website/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import Navbar from './components/Navbar.jsx'
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <Navbar />
+    </div>
+  )
+}
+
+export default App
